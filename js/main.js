d78bdd695305007464ea64b2a197cfbb83ef5286
osamaomran491 <osamaomran170@gmail.com>
2024-08-22 20:51:48 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+var siteLists;
+localStorage.getItem("siteLists") == null ? (siteLists = []) : siteLists = (JSON.parse(localStorage.getItem("siteLists")));
+ showLists(siteLists);
+var SiteName = document.getElementById("SiteName");
+var SiteURL = document.getElementById("SiteURL");
+var submitButton = document.getElementById("submitButton");
+function Submit() {
+    if(siteLists){
+     var list = {
+         Name: SiteName.value,
+         Link: SiteURL.value,
+     };
+     siteLists.push(list)
+     localStorageUpdate()
+     showLists(siteLists);
+     clearInputs();
+ }
+ }
+ function  showLists (siteLists){
+    var cartona =``
+   for( var i = 0 ; i< siteLists.length; i++){
+
+   cartona += ` <tr>
+    <td>${i}</td>
+    <td>${siteLists[i].newName ? siteLists[i].newName : siteLists[i].Name}</td>
+    <td>${siteLists[i].Link}</td>
+    <td><button onclick="visitSite(${siteLists[i].link})" class="btn btn-warning">Visit</button></td>
+    <td><button onclick="deleteList(${i})" class="btn btn-danger">Delete</button></td>
+   </tr>
+`
+   }
+  document.getElementById("detials").innerHTML = cartona;
+}
+function deleteList(index){
+    siteLists.splice(index,1)
+    localStorageUpdate()
+    showLists(siteLists);
+    }
+    function clearInputs(){
+        SiteName.value= '';
+        SiteURL.value= '';
+    }
+    function visitSite(index){
+        SiteName=siteLists[index].Name
+        SiteURL=siteLists[index].Link
+    }
